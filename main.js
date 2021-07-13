5655529adef89d633687701015c8faf1d28416a0
Noblegamer2809 <85750071+Noblegamer2809@users.noreply.github.com>
2021-07-13 18:48:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function changeimage(){
+    document.getElementById("hello").src="image 2.jfif";
+    document.getElementById("h1").style.color="red"
+    document.getElementById("p").style.color="pink"
+}
